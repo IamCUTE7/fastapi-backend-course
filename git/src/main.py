df355482d8d8234c7133af4527cb9efb378c3fe3
Michael <something@gmail.com>
2026-03-09 21:41:23 +0000

    all errors fixed

--- git/src/main.py
+++ git/src/main.py
@@ -14,11 +14,7 @@ def load_books(filename='library.json'):
         except json.JSONDecodeError:
             return []
 
-<<<<<<< HEAD
 def save_books(books, filename='library.json'):
-=======
-def saving_books(books, filename='library.json'):
->>>>>>> new/second-branch
     """
     Сохранение списка книг в JSON-файл.
     """
@@ -98,11 +94,7 @@ def main():
             # Получаем новый список с добавленной книгой
             new_books = add_book(books, title, author, year)
             books = new_books  # Обновляем переменную, чтобы сохранить изменения
-<<<<<<< HEAD
             save_books(books)  # Сразу сохраняем в файл
-=======
-            saving_books(books)  # Сразу сохраняем в файл
->>>>>>> new/second-branch
             print("Книга добавлена!")
 
         elif choice == '3':
@@ -110,15 +102,10 @@ def main():
             title_to_remove = input("Введите название книги, которую хотите удалить: ").strip()
 
             new_books = remove_book(books, title_to_remove)
-<<<<<<< HEAD
             if len(new_books) < len(books):
                 books = new_books
                 save_books(books)
-=======
-            if len(new_books) > len(books):
-                books = new_books
-                saving_books(books)
->>>>>>> new/second-branch
+
                 print("Книга удалена!")
             else:
                 print("Книга с таким названием не найдена.")
@@ -133,26 +120,13 @@ def main():
             else:
                 print("Ничего не найдено.")
 
-<<<<<<< HEAD
         elif choice == '5':
-=======
-        elif choice == '6':
->>>>>>> new/second-branch
             print("Выход из программы.")
             break
 
         else:
             print("Некорректный ввод. Попробуйте ещё раз.")
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     main()
-=======
 
-
-
-
-
-if __name__ == "__main__":
-    main()
->>>>>>> new/second-branch
